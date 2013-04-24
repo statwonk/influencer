4300f3c7f381d6c4384f01e57de695cdf9d65c90
cpeter9 <Peters>
2013-04-24 12:37:49 -0500

    Added predictions.

--- influencer.R
+++ influencer.R
@@ -4,7 +4,7 @@
 # setwd("C:/Users/Chris/Desktop/RCode/influencer/influencer/")
 setwd("C:/R_stuff/influencer/influencer")
 data <- read.csv("train.csv")
-# test <- read.csv("test.csv")
+test <- read.csv("test.csv")
 
 pairs(data[ , 1:5])
 
@@ -19,6 +19,12 @@ ada_1 <- gbm(Choice ~ ., distribution = "adaboost",
              shrinkage = 0.005,
              interaction.depth = 6, train.fraction = 1, cv.folds = 4)
 
+ada_preds_cv <- ifelse(predict(ada_1, data[-train, ], type = "response") < 0, 0, 1)
+ada_preds_test <- ifelse(predict(ada_1, test, type = "link") < 0, 0, 1)
+
+write.csv(ada_preds_test, "preds_1.csv", row.names = F)
+
+confusionMatrix(ada_preds_cv, data[-train, 1])
 
 # install.packages("caret")
 # install.packages("e1071")
@@ -35,8 +41,31 @@ ctrl <- trainControl(method = "cv",
                      classProbs = TRUE,
                      summaryFunction = twoClassSummary)
 
+
 data$Choice <- as.factor(data$Choice)
 levels(data$Choice) <- c("X0", "X1")
+
+library(randomForest)
+
+tuneRF(data[inTrain$Resample1, -1], data[inTrain$Resample1, 1], 5, ntreeTry = 50, stepFactor = 2, improve = 0.01,
+       trace=TRUE, plot=TRUE, doBest = T)
+
+rfGrid <- expand.grid(.mtry = c(2, 3, 4))
+
+rf_1 <- train(Choice ~ .,
+                data = data[inTrain$Resample1, ],
+                method = "rf",
+                metric = "ROC",
+                tuneLength = 3,
+                trControl = ctrl,
+                tuneGrid = rfGrid)
+
+rf_classes <- predict(rf_1, newdata = data[-inTrain$Resample1, ])
+confusionMatrix(rf_classes, data[-inTrain$Resample1, 1])
+
+rf_preds_test <- predict(rf_1, test, type = "prob")
+
+write.csv(rf_preds_test[2], "preds_2.csv", row.names = F)
 
 tree_1 <- train(Choice ~ .,
           data = data[inTrain$Resample1, ],
